a29a10a260d836dbcca55c433d19450b2beea9fb
Marvin Macharia <machariamarvin625@gmail.com>
2022-08-30 00:11:38 +0300

    Create top bar and nav

--- js/app.js
+++ js/app.js
@@ -5,4 +5,16 @@ $(function () {
         $("#preloader").on(500).fadeOut();
         $(".preloader").on(600).fadeOut("slow");
     });
+
+    // Affix
+
+    $('.megamenu').affix({
+        offset: {
+            top: 0,
+            bottom: function() {
+                return (this.bottom = $('.footer').outerHeight(true))
+            }
+        }
+    })
+
 })
